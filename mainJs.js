44c2b7af018934764117472cbad259a38199a9f8
vasanth134 <crazyvasanth004@gmail.com>
2024-01-22 18:11:32 +0530

    Add event handlers and update HTML elements
    This commit adds event handlers to the email input and submit button in the mainPage.html file. It also updates the JavaScript code in mainJs.js to handle these events and update the "thanks" element accordingly.

---
+++ mainJs.js
@@ -0,0 +1,14 @@
+const congrats = () => {
+  
+    let elemnet = (document.getElementById("thanks").innerHTML = "Welcome"); 
+  
+};
+const thank = () => {
+    document.getElementById("thanks").innerHTML = "Click Me";
+}
+const thanks = () => {
+    document.getElementById("thanks").innerHTML = "Thanks";
+}
+const none = () => {
+    document.getElementById("thanks").innerHTML = "     ";
+}
